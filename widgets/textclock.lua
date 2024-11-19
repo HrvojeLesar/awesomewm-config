420566adc87637ded1ae1cd7ef09182b3942e46b
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2024-11-19 21:50:17 +0100

    calendar

--- widgets/textclock.lua
+++ widgets/textclock.lua
@@ -1,9 +1,24 @@
 local wibox = require("wibox")
 
-local textclock = {
+local textclock = wibox.widget({
     format = "%a %d %b %H:%M:%S",
     widget = wibox.widget.textclock,
     refresh = 1
-}
+})
+
+local calendar_widget = require("awesome-wm-widgets.calendar-widget.calendar")
+
+local cw = calendar_widget({
+    placement = "top_right",
+    auto_hide = true,
+})
+
+textclock:connect_signal("button::press",
+    function(_, _, _, button)
+        if button == 1 then
+            cw.toggle()
+        end
+    end
+)
 
 return textclock
